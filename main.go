00512c0b022ba74b5f96eebfc5da21c56b5c1424
tetsu5555 <tetete1118@gmail.com>
2022-05-31 18:36:50 +0900

    add scraping

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html/charset"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/saintfish/chardet"
+)
+
+func main() {
+	url := "https://beam.apache.org/get-started/downloads/#releases"
+	searchWord := "change"
+
+	urls := getUrlsByWord(url, "Release notes")
+
+	// それぞれのurlをvisitする
+	for _, v := range urls {
+		visitUrl(v, searchWord)
+	}
+}
+
+func getDocumentByUrl(url string) *goquery.Document {
+	// Getリクエストでレスポンス取得
+	res, _ := http.Get(url)
+	defer res.Body.Close()
+
+	// Body内を読み取り
+	buffer, _ := ioutil.ReadAll(res.Body)
+
+	// 文字コードを判定
+	detector := chardet.NewTextDetector()
+	detectResult, _ := detector.DetectBest(buffer)
+	// fmt.Println(detectResult.Charset)
+	// => UTF-8
+
+	// 文字コードの変換
+	bufferReader := bytes.NewReader(buffer)
+	reader, _ := charset.NewReaderLabel(detectResult.Charset, bufferReader)
+
+	// HTMLをパース
+	document, _ := goquery.NewDocumentFromReader(reader)
+
+	return document
+}
+
+// textにwordを含むaタグのurlを抽出する
+func getUrlsByWord(url string, word string) []string {
+	urls := []string{}
+
+	document := getDocumentByUrl(url)
+
+	// urlを抜き出し
+	document.Find("a").Each(func(i int, s *goquery.Selection) {
+		title := s.Text()
+		if title == word {
+			val, exists := s.Attr("href")
+			if exists {
+				fmt.Println(val)
+			}
+			urls = append(urls, val)
+		}
+	})
+
+	return urls
+}
+
+func visitUrl(url string, word string) {
+	document := getDocumentByUrl(url)
+
+	fmt.Println(url, strings.Contains(document.Text(), word))
+}
